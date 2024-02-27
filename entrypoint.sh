f2aae0e46bc5113c84e30c08e714e30476183a13
robert-min <robertmin522@gmail.com>
2024-02-27 10:11:03 +0900

    fix: Dockerimage entrypoint.sh with venv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
+python3 -m venv .venv
+source .venv/bin/activate
+
 # instal package
 pip install -r requirements.txt
+
+# torch
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
 # run backend server
 # uvicorn app:app --host 0.0.0.0 --port 8000
