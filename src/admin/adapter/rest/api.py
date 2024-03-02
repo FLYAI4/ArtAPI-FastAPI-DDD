859446bc07cc3bfae54fec7f4fa4fe6f46adc2d1
robert-min <robertmin522@gmail.com>
2024-03-02 14:24:05 +0900

    feature: Connect  admin/infra/container to api

--- src/admin/adapter/rest/api.py
+++ src/admin/adapter/rest/api.py
@@ -1,25 +1,21 @@
-from azure.storage.blob import BlobServiceClient
+from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, UploadFile, File, Depends, Header
 from src.admin.adapter.rest.response import GeneratedContentResponse
 from src.admin.application.command import AdminCommandUseCase
-from src.shared_kernel.infra.database.connection import MongoManager, BlobStorageManager
-
+from src.shared_kernel.infra.container import AppContainer
 
 admin = APIRouter(prefix="/admin")
 
 
 @admin.post("/content")
+@inject
 async def generate_content(
     file: UploadFile = File(...),
     imagename: str = Header(...),
-    mongo_session: any = Depends(MongoManager.get_session),
-    azure_blob_session: BlobServiceClient = Depends(
-        BlobStorageManager.get_session)
+    admin_command: AdminCommandUseCase = Depends(
+        Provide[AppContainer.admin.admin_command])
 ):
-    command = AdminCommandUseCase(
-        mongo_session, azure_blob_session
-    )
-    result = await command.generate_content(imagename, file)
+    result = await admin_command.generate_content(imagename, file)
     return GeneratedContentResponse(
         generated_content_name=result
     ).build()
--- src/shared_kernel/infra/container.py
+++ src/shared_kernel/infra/container.py
@@ -1,5 +1,6 @@
 from dependency_injector import containers, providers
 from src.account.infra.container import AccountContainer
+from src.admin.infra.container import AdminContainer
 
 
 class AppContainer(containers.DeclarativeContainer):
@@ -7,6 +8,8 @@ class AppContainer(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
         modules=[
             "src.account.adapter.rest.api",
+            "src.admin.adapter.rest.api"
         ]
     )
     account = providers.Container(AccountContainer)
+    admin = providers.Container(AdminContainer)
